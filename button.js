316dc0c3df5a444dfc9a1707043dfe45341adcb1
boccos <145885073+boccos@users.noreply.github.com>
2025-10-04 20:25:01 -0400

    Basic book logging functionality added

---
+++ button.js
@@ -0,0 +1,25 @@
+const addBook = document.getElementById("openButton")
+const dialog = document.querySelector("dialog")
+const form = document.getElementById("newBookForm")
+
+addBook.addEventListener("click", () => dialog.show())
+form.addEventListener("submit", (submission) => {
+    submission.preventDefault()
+    const title = form.title.value
+    const author = form.author.value
+    const pages = form.pages.value
+    const year = form.year.value
+    const type = form.type.value
+    const isRead = form.isRead.checked
+
+    const newBook = new Book(title, author, pages, year, type, isRead)
+
+    addBookToLibrary(newBook)
+
+    createCard(myLibrary)
+
+    form.reset();
+    dialog.close()
+})
+
+// const cancelBook = document.getElementById("closeButton")
---
+++ library.js
@@ -0,0 +1,93 @@
+const myLibrary = [];
+
+
+function Book(title, author, pages, year, type, isRead) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.year = year
+  this.type = type
+  this.isRead = isRead
+  this.id = crypto.randomUUID()
+}
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook)
+}
+
+function createCard(catalogue){
+  const container = document.getElementById("container")
+  container.innerHTML = ""
+  catalogue.forEach(book => {
+    container.appendChild(getInfo(book))
+  })
+}
+
+function getInfo(book){
+  const card = document.createElement("div")
+  card.setAttribute("class", "book_card")
+
+  const title = document.createElement("div")
+  title.setAttribute("class", "title")
+  title.textContent = `Title: ${book.title}`
+  card.appendChild(title)
+
+  const author = document.createElement("div")
+  author.setAttribute("class", "author")
+  author.textContent = `Author: ${book.author}`
+  card.appendChild(author)
+  
+  const pages = document.createElement("div")
+  pages.setAttribute("class", "pages")
+  pages.textContent = `Pages: ${book.pages}`
+  card.appendChild(pages)
+
+  const year = document.createElement("div")
+  year.setAttribute("class", "year")
+  year.textContent = `Year: ${book.year}`
+  card.appendChild(year)
+
+  const type = document.createElement("div")
+  type.setAttribute("class", "type")
+  type.textContent = `Type: ${book.type}`
+  card.appendChild(type)
+
+
+  const checkboxLabel = document.createElement('label');
+  checkboxLabel.textContent = "Is Read: ";
+  
+  const checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.checked = book.isRead;
+
+  checkbox.addEventListener('change', () => 
+  {
+    book.isRead = checkbox.checked;
+  });
+
+  checkboxLabel.appendChild(checkbox);
+  card.appendChild(checkboxLabel);
+
+  const book_id = document.createElement("div")
+  book_id.setAttribute("class", "book_id")
+  book_id.textContent = `ID: ${book.id}`
+  card.appendChild(book_id)
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Remove";
+
+  deleteBtn.addEventListener("click", () => {
+    const index = myLibrary.findIndex(b => b.id === book.id);
+    if (index !== -1) {
+        myLibrary.splice(index, 1);
+    }
+
+    card.remove();
+  });
+
+
+  card.appendChild(deleteBtn);
+
+
+  return card;
+}
